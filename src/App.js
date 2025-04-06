592db3ea3703b37d93c397c892105b371c63bc6c
LaibaMubasher <laibamubasher485@gmail.com>
2025-04-06 22:36:55 +0500

    Improved the structure of the website

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Signup from './pages/signup';
 import SelectMood from './pages/selectmood';
 import Recommendations from './pages/recommendations';
 import TopSongs from './pages/topsongs';
+import Dashboard from './pages/dashboard';
+import Profile from './pages/Profile';
 
 
 function App() {
@@ -14,9 +16,11 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/select-mood" element={<SelectMood />} />
-        <Route path="/recommendations" element={<Recommendations />} />
+        <Route path="/recommendations/:mood" element={<Recommendations />} />
         <Route path="/top-songs" element={<TopSongs />} />
         <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
