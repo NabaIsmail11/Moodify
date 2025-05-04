f8e02dfcf82dd126b520927599ec704ac4a39d38
NabaIsmail11 <165937829+NabaIsmail11@users.noreply.github.com>
2025-05-04 19:44:12 +0500

    route to manage playlists of user

---
+++ backend/routes/playlists.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import {
+  createPlaylist,
+  getUserPlaylists,
+  getPlaylistById,
+  updatePlaylist,
+  deletePlaylist,
+  addSongToPlaylist,
+  removeSongFromPlaylist,
+  getUserProfile,
+  updateUserActivity
+} from '../controllers/playlistcontroller.js';
+import { authenticateUser } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.use(authenticateUser);
+
+router.get('/profile', getUserProfile);
+router.post('/activity', updateUserActivity);
+
+router.route('/')
+  .post(createPlaylist)
+  .get(getUserPlaylists);
+
+router.route('/:id')
+  .get(getPlaylistById)
+  .put(updatePlaylist)
+  .delete(deletePlaylist);
+
+router.post('/:id/songs', addSongToPlaylist);
+router.delete('/:id/songs/:songId', removeSongFromPlaylist);
+
+export default router;
