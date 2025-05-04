f3528c1ad18998295501ce87b5a209b1d65d87cf
NabaIsmail11 <165937829+NabaIsmail11@users.noreply.github.com>
2025-05-04 19:21:04 +0500

    Create profile.js

---
+++ frontend/src/pages/profile.js
@@ -0,0 +1,2 @@
+// The Profile component displays user profile data, including top artists, top tracks, and user playlists.
+// It allows the user to edit their profile, create and delete playlists, and view detailed information about their music preferences.
